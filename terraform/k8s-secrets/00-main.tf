4d4b4b16effab88ebae18ec636e100cfcd014ea2
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-01-22 11:58:03 +0100

    feat: automating secrets sync between SecretsManager and k8s (#88)

---
+++ terraform/k8s-secrets/00-main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = "~> 1.8.0"
+
+  backend "s3" {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.46.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = var.tags
+  }
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.eks_cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_caller_identity" "current" {}
